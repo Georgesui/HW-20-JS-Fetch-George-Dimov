2f9fd0ea82260840cea27af9f23ae0eda4c23947
Georgesui <gdimov445@gmail.com>
2022-06-13 15:01:06 +0300

    my commit

--- script.js
+++ script.js
@@ -21,27 +21,29 @@ const ourAlbumsOnPage = document.querySelector('.list-albums');
 const ourPhotosOnPage = document.querySelector('.list-img');
 
 async function addAlbums(albums) {
-	ourAlbumsOnPage.innerHTML = await albums.map(album =>
-		`<li data-id="${album.id}">${album.id}. "${album.title}"</li>`).join('')
+	ourAlbumsOnPage.innerHTML = albums.map(album => `<li data-id="${album.id}">${album.id}. "${album.title}"</li>`).join('')
 }
 
 async function addPhotos(photo) {
-	ourPhotosOnPage.innerHTML = await photo.map(photos =>
-		`<li data-id="${photos.id}"><p class='photo-text'>${photos.id}.</p> <img src='${photos.url}' </li>`).join('')
+	ourPhotosOnPage.innerHTML = photo.map(photos =>
+		`<li data-id="${photos.id}"><a href='${photos.url}' class='link-photos'><p class='photo-text'>${photos.id}. </p> <img src='${photos.thumbnailUrl}'</a></li>`).join('')
 }
 
-getAlbums()
-	.then((albums) => addAlbums(albums))
-	.then(() => getPhotos(1))
-	.then((photos) => addPhotos(photos))
-	.catch(error => console.warn(error))
+async function addPhotosAndAlbumsOnPage() {
+	try {
+		const ourAlbums = await getAlbums();
+		await addAlbums(ourAlbums);
+		const ourPhotos = await getPhotos(1)
+		await addPhotos(ourPhotos)
+	} catch {
+		console.warn('Error')
+	}
+}
+
+addPhotosAndAlbumsOnPage()
 
 ourAlbumsOnPage.addEventListener('click', (event) => {
 	getPhotos(event.target.dataset.id)
 		.then((photos) => addPhotos(photos))
 		.catch(error => console.warn(error))
 })
-
-// let li = document.createElement('li');
-// 	document.body.children[0].childNodes[1].append(li)
-// 	li.innerHTML = blob;
